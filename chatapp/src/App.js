e661a30f0672331af0684be93f562a5fd5e3085d
Joshua Li <joshuazli2020@gmail.com>
2022-12-11 18:31:02 +1000

    importing firebase hooks and adding in config

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBzGMX_cIG2eK8ozU79BfNg2rdktOSY1lI",
+  authDomain: "chatroom-c75b0.firebaseapp.com",
+  projectId: "chatroom-c75b0",
+  storageBucket: "chatroom-c75b0.appspot.com",
+  messagingSenderId: "189973780269",
+  appId: "1:189973780269:web:b88c16a4ebc3a4aacf65a4",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
